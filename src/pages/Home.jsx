f8c0f7338a116dafa1eef66bfafe363255044125
Heechem <hichempeter@gmail.com>
2023-08-29 17:10:22 -0600

    Starter  config commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import {
+  headContentAnimation,
+  headContainerAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
